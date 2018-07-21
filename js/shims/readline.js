2dd2e99a286c5057d180bf1eafc2fc4148364ee2
shalvah <diakon.ng@gmail.com>
2018-07-21 23:09:48 +0100

    Add shim for Readline - to get Inquirer working

---
+++ js/shims/readline.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  createInterface({ input, output }) {
+      // so we don't redefine these properties
+      if (input.readlineified) {
+          return input;
+      }
+      // normally, input and output should be equal to the same xterm.Terminal instance
+      input.input = input;
+      input.output = input;
+      input.output.mute = function () {};
+      input.output.unmute = function () {};
+      input.output.end = function () {};
+      input.pause = function () {};
+      input.resume = function () {};
+      input.close = function () {};
+      input.setPrompt = function () {};
+      input._getCursorPos = function () {
+          return {
+              cols: input.buffer.x,
+              rows: input.buffer.y
+          };
+      };
+      input.removeListener = input.off.bind(input);
+
+      Object.defineProperty(input, 'line', {
+          get: function () {
+              return input.textarea.value;
+          }
+      });
+      input.readlineified = true;
+      return input;
+  }
+};
