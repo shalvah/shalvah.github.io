d2bce3ac8bf979633647c4d2fa3c4f79d4b585d6
shalvah <diakon.ng@gmail.com>
2018-07-24 21:57:29 +0100

    Switch from full screen mode to fit mode

--- js/index.js
+++ js/index.js
@@ -1,13 +1,44 @@
+const chalk = require('chalk');
+const PROMPT = `${chalk.red('root')} @ ${chalk.blue('/')} > `;
+
 const term = createTerminal();
 setUpTermEventHandlers(term);
 setUpShims(term);
 setUpTermUi(term);
 
+let commandHistory = [];
+let historyIndex = 0;
+
 console.log(process);
 window.term = term;
 
 function setUpTermEventHandlers(term) {
+
     term.on('key', (key, ev) => {
+        if (process.running) {
+            if (ev.ctrlKey && ev.key === 'c') {
+                term.emit('SIGINT');
+                return;
+            }
+
+            if (ev.key === 'ArrowUp') {
+                ev.name = 'up';
+            } else if (ev.key === 'ArrowDown') {
+                ev.name = 'down';
+            } else if (Number(ev.key)) {
+                ev.value = ev.key;
+            }
+            console.log(term.textarea.value)
+            if (ev.key === 'Enter') {
+                term.emit('line');
+                term.emit('line');
+                term.emit('line');
+            } else {
+                term.emit('keypress', key, ev);
+            }
+            return;
+        }
+
         const printable = (
             !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey
         );
@@ -19,12 +50,22 @@ function setUpTermEventHandlers(term) {
             if (term.buffer.x > PROMPT.length) {
                 term.write('\b \b');
             }
-            const value = term.textarea.value
+            const value = term.textarea.value;
             term.textarea.value = value.slice(0, value.length - 1);
             console.log(term.textarea.value);
+        } else if (ev.key === 'ArrowUp') {
+            if (historyIndex > 0) {
+                showHistoryItem(--historyIndex);
+                console.log(historyIndex)
+            }
+        } else if (ev.key === 'ArrowDown') {
+            if (historyIndex < commandHistory.length) {
+                showHistoryItem(++historyIndex);
+                console.log(historyIndex)
+            }
         } else if (printable) {
             if(term.textarea.value.split(/\s+/).length < 2 && key !== ' ') {
-                term.write(require('chalk').cyan(key));
+                term.write(chalk.cyan(key));
             } else {
                 term.write(key);
             }
@@ -42,8 +83,12 @@ function setUpTermEventHandlers(term) {
             return;
         }
 
+        // output should start from the next line
         term.write('\r\n');
-        if (argv[0].toLowerCase() !== 'shalvah') {
+        process.running = true;
+        historyIndex = commandHistory.push(line.text);
+        const recognisedCommands = ['shalvah'];
+        if (!recognisedCommands.includes(argv[0])) {
             term.write('Unknown command: ' + argv[0]);
             term.emit('line-processed');
             return;
@@ -55,6 +100,7 @@ function setUpTermEventHandlers(term) {
             .parse([''].concat(argv));
         const shalvah = require('shalvah');
         const inquirer = require('inquirer');
+        term.writeln(shalvah.bio);
         inquirer.prompt({
             name: 'link',
             type: 'list',
@@ -64,14 +110,16 @@ function setUpTermEventHandlers(term) {
                 'value': 'mailto:' + shalvah.email
             })
         }).then(answers => {
-            console.log(`Opening ${answers.link}`);
+            term.writeln(`Opening ${answers.link}`);
             window.open(answers.link);
+            term.emit('line-processed');
         });
 
     });
 
     term.on('line-processed', () => {
         term.writeWithPrompt('');
+        process.running = false;
     });
 }
 
@@ -106,6 +154,20 @@ function setUpShims(term) {
     };
 
     /*
+     * for inquirer.js to show the choice selection pointer (list prompt) properly
+     */
+    process.platform = 'win32';
+
+    /*
+     * For inquirer.js to exit when Ctrl-C is pressed
+     */
+    process.kill = () => {
+      process.running = false;
+      term.writeln();
+      term.writeWithPrompt('');
+    };
+
+    /*
      * Used by Commander for error responses
      * This is covers only my specific use cases
      * and tries to maintain regular console logs for other packages
@@ -124,31 +186,42 @@ function setUpShims(term) {
 
 function createTerminal() {
     const Terminal = require('xterm').Terminal;
-    const fullscreen = require('xterm/lib/addons/fullscreen/fullscreen');
-    Terminal.applyAddon(fullscreen);
+    const fit = require('xterm/lib/addons/fit/fit');
+    Terminal.applyAddon(fit);
 
     const term = new Terminal({
         cursorBlink: true,
         convertEol: true,
+        fontFamily: 'Consolas',
+
     });
 
-    const PROMPT = '> ';
     term.writeWithPrompt = function (...args) {
         this.writeln(...args);
         this.write(PROMPT);
+        this.focus();
+        this.showCursor();
     };
     term.newLine = function () {
         let value = this.textarea.value;
         this.textarea.value = "";
         this.emit('newline', { text: value });
     };
+
     return term;
 }
 
 function setUpTermUi(term) {
     term.open(document.getElementById('terminal'));
-    term.toggleFullScreen();
-    term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+    term.fit();
     term.writeWithPrompt('');
     term.focus();
+}
+
+function showHistoryItem(index) {
+    let text = commandHistory[index] === undefined ? '' : commandHistory[index];
+    while (term.buffer.x > PROMPT.length) {
+        term.write('\b \b');
+    }
+    term.write(text);
 }
