fd525e3e3f47e689771fc6e74dd0d089dfa018ec
shalvah <diakon.ng@gmail.com>
2018-07-24 22:42:59 +0100

    Aliasify iconv-lite, so Inquirer.js works

--- browserify.js
+++ browserify.js
@@ -9,8 +9,10 @@ if (process.argv.includes('--debug')) {
 browserify('js/index.js', options)
     .transform('aliasify', {
         aliases: {
-            'readline': './js/shims/readline.js',
+            readline: './js/shims/readline.js',
             'supports-color': './js/shims/supports-color.js',
+            // problematic module that I'm not using in my site
+            'iconv-lite': './node_modules/browserify/lib/_empty.js'
         },
         verbose: true,
         global: true
